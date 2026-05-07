ca6c4d72384283d2db38f9c9ba3b0164f0d69961
Kian Hirani <Kian.r.hirani@student.uts.edu.au>
2026-05-07 10:20:47 +1000

    Initial Commit

---
+++ DailyHabitTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DailyHabitTracker
+//
+//  Created by Kian Hirani on 7/5/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DailyHabitTracker/DailyHabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyHabitTrackerApp.swift
+//  DailyHabitTracker
+//
+//  Created by Kian Hirani on 7/5/2026.
+//
+
+import SwiftUI
+
+@main
+struct DailyHabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
