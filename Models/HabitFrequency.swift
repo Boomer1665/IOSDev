18d119472d1769c8b30a9ca7a2c965e9aafdf8fc
thatguythere22 <Zade.elsaddik@student.uts.edu.au>
2026-05-10 23:43:08 +1000

    Update HabitFrequency.swift

--- Models/HabitFrequency.swift
+++ Models/HabitFrequency.swift
@@ -7,7 +7,7 @@
 
 
 import Foundation
-
+// Defines how often a habit is expected and is used for display labels and future scheduling logic.
 enum HabitFrequency: String, Codable, CaseIterable {
     case daily = "Daily"
     case threeAWeek = "3x Week"
